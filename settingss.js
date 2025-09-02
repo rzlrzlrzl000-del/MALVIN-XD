3c0867075a6327fbef064eb36470f380ec6986a4
rzlrzlrzl000-del <rzlrzlrzl000@gmail.com>
2025-09-02 22:35:59 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~R28WCA5Y#W_mV1rQlA6PeWxxLgD9dVb_xDMp_IvEulAkE5Lr553E",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "918078182087", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
